316cea7bc1ed5d9e29d7b3e933d193cde622251d
raunaqtri1 <raunaq.trivedi01@gmail.com>
2019-11-25 19:57:42 -0800

    notify of training failure/completion

--- lol_player.py
+++ lol_player.py
@@ -5,6 +5,8 @@ from stable_baselines.common.policies import MlpLstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
+from notify_run import Notify
+
 def main (time): 
     env = DummyVecEnv([lambda: gym.make('LoL-v0')])
     model = PPO2(MlpLstmPolicy, env, verbose=1, nminibatches=1)
@@ -12,4 +14,12 @@ def main (time):
     model.save('ppo_lol')
 
 if __name__ == "__main__":
-    main(100000)
+    notify = Notify(endpoint='https://notify.run/1GQn88vSML1rmxdz')
+    try:
+        main(100000)
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        notify.send('Training Failed for LoL-v0')
+    else:
+        notify.send('Training Completed for LoL-v0')
