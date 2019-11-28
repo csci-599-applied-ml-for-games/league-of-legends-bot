7298b7988c487cdcfa57d3ad5a401775d8f20d9b
bkeslin <bkeslin@ucsc.edu>
2019-11-27 18:07:39 -0800

    Version 1 attempting to add intermittent saves

--- lol_player.py
+++ lol_player.py
@@ -7,16 +7,24 @@ from stable_baselines import PPO2
 
 from notify_run import Notify
 
-def main (time): 
-    env = DummyVecEnv([lambda: gym.make('LoL-v0')])
-    model = PPO2(MlpLstmPolicy, env, verbose=1, nminibatches=1)
+def update model (model = None, time):
+    if model == None :
+        env = DummyVecEnv([lambda: gym.make('LoL-v0')])
+        model = PPO2(MlpLstmPolicy, env, verbose=1, nminibatches=1)
+    
     model.learn(total_timesteps=time)
     model.save('ppo_lol')
+    
+    return model
+   
 
 if __name__ == "__main__":
     notify = Notify(endpoint='https://notify.run/1GQn88vSML1rmxdz')
     try:
-        main(100000)
+        model = None
+        for i in range (10000) :
+            model = update_model(model, 10)
+
     except (KeyboardInterrupt, SystemExit):
         raise
     except:
